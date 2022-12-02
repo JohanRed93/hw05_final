d0f082b60d22a73d01cc8239e33c8dda87dc878c
Горяев Иван <johanred1993@gmail.com>
2022-12-03 02:25:43 +0300

    Final done PEP

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -7,7 +7,7 @@ app_name = 'posts'
 urlpatterns = [
     path('create/', views.post_create, name='post_create'),
     path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
-    path('posts/<int:post_id>/comment/', 
+    path('posts/<int:post_id>/comment/',
          views.add_comment, name='add_comment'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
     path('profile/<str:username>/', views.profile, name='profile'),
